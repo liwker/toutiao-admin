e43523a2dd9e830eb43f79d502a686a91dc67b68
Liwker <2385756605@qq.com>
2021-09-09 20:14:09 +0800

    个人设置-上传功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'no-unused-vars': process.env.NODE_ENV === 'production' ? 'warn' : 'off', // 关闭 var 定义校验
+    // 'semi': 'off' // 关闭 分号 校验
   }
 }
--- src/views/settings/index.vue
+++ src/views/settings/index.vue
@@ -10,23 +10,23 @@
         <!-- /面包屑 -->
       </div>
       <el-row>
-        <el-col :span="15">
+        <el-col :span="16">
           <!-- 个人信息表单 -->
           <el-form ref="form" :model="form" label-width="70px">
             <el-form-item label="编号">
-              123
+              {{ user.id }}
             </el-form-item>
             <el-form-item label="手机">
-              456
+              {{ user.mobile }}
             </el-form-item>
             <el-form-item label="媒体名称">
-              <el-input v-model="form.name"></el-input>
+              <el-input v-model="user.name"></el-input>
             </el-form-item>
             <el-form-item label="媒体介绍">
-              <el-input type="textarea" v-model="form.desc"></el-input>
+              <el-input type="textarea" v-model="user.intro"></el-input>
             </el-form-item>
             <el-form-item label="邮箱">
-              <el-input v-model="form.name"></el-input>
+              <el-input v-model="user.email"></el-input>
             </el-form-item>
 
             <el-form-item>
@@ -35,22 +35,45 @@
           </el-form>
         </el-col>
         <!-- 头像 -->
-        <el-col :span="4">
-          <el-avatar
-            shape="square"
-            :size="150"
-            :fit="fit"
-            src="http://toutiao-img.itheima.net/Fve7Gg2XmCsjDXcZjyY_yr-2FAE2"
-          ></el-avatar>
-          <p>点击修改头像</p>
+        <el-col :span="6" :offset="2">
+          <label for="file">
+            <el-avatar
+              shape="square"
+              :size="150"
+              fit="fit"
+              :src="user.photo"
+            ></el-avatar>
+            <p>点击修改头像</p>
+          </label>
+          <input
+            type="file"
+            id="file"
+            ref="file"
+            hidden
+            @change="onFileChange"
+          />
         </el-col>
       </el-row>
-
     </el-card>
+
+    <el-dialog
+      title="修改头像"
+      :visible.sync="dialogVisible"
+      append-to-body
+    >
+      <img :src="previewImage" alt="">
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+
   </div>
 </template>
 
 <script>
+import { getUserProfile } from '@/api/user'
+
 export default {
   name: 'SettingsIndex',
   components: {},
@@ -66,16 +89,48 @@ export default {
         type: [],
         resource: '',
         desc: ''
-      }
+      },
+      user: { // 用户资料
+        email: '',
+        id: null,
+        intro: '',
+        mobile: '',
+        name: '',
+        photo: ''
+      },
+      dialogVisible: false, // 控制图片弹出层
+      previewImage: '' // 预览图片
     }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.loadUser()
+  },
   mounted () {},
   methods: {
     onSubmit () {
       console.log('submit!')
+    },
+    // 获取用户资料
+    loadUser () {
+      getUserProfile().then(res => {
+        // console.log(res)
+        this.user = res.data.data
+      })
+    },
+    // 上传头像
+    onFileChange () {
+      // 处理图片预览
+      const file = this.$refs.file
+      // 生成blob地址
+      const blobData = window.URL.createObjectURL(file.files[0])
+      this.previewImage = blobData
+
+      // 展示弹出层，预览图片
+      this.dialogVisible = true
+      // 清空文件
+      this.$refs.file.value = ''
     }
   }
 }
